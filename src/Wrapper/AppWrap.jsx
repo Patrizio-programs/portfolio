64d08b2d7b0ffd8bc7b0ff9d76da7c178b733bda
Patrizio-programs <patickmedley@gmail.com>
2022-06-18 12:04:46 -0400

    update

--- src/Wrapper/AppWrap.jsx
+++ src/Wrapper/AppWrap.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import SocialMedia from "../components/SocialMedia";
 
@@ -19,26 +18,4 @@ const AppWrap = (Component, idName, classNames) =>
     );
   };
 
-=======
-import React from "react";
-import SocialMedia from "../components/SocialMedia";
-
-const AppWrap = (Component, idName, classNames) =>
-  function HOC() {
-    return (
-      <div id={idName} className={`app__container ${classNames}`}>
-        <SocialMedia />
-        <div className="app__wrapper app__flex">
-          <Component />
-
-          <div className="copyright">
-            <p className="p-text">@2022 Patrick</p>
-            <p className="p-text">All rights reserved</p>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
->>>>>>> a6e474b2306bb5af9cdbf466e4f260ea62e8664d
 export default AppWrap;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logo from '../assets/logo.png';
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
@@ -62,69 +61,4 @@ const Navbar = () => {
   )
 }
 
-=======
-import logo from '../assets/logo.png';
-import React, { useState } from 'react';
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
-import './Navbar.scss';
-
-const Navbar = () => {
-    const [toggle, setToggle] = useState(false);
-  return (
-    <nav className='app__navbar'>
-        <div className='app__navbar-logo'>
-        <img src={logo} alt="" />
-        </div>
-        <ul className="app__navbar-links">
-       
-          <li className="app__flex p-text">
-           
-            <a href='https://patriziothedev.com/'> Home</a>
-        </li>
-        
-        <li className="app__flex p-text">
-           
-           <a href='https://patriziothedev.com/'>About Me</a>
-        </li>
-        
-       
-  
-      </ul>
-
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              
-                <li>
-                  <a href='https://patriziothedev.com/' onClick={() => setToggle(false)}>Home
-                  
-                </a>
-                
-              </li>
-              
-              
-              <li>
-                  <a href='https://patriziothedev.com/' onClick={() => setToggle(false)}>About Me
-                  
-                </a>
-                
-                </li>
-          
-            </ul>
-          </motion.div>
-        )}
-      </div>
-    </nav>
-  )
-}
-
->>>>>>> a6e474b2306bb5af9cdbf466e4f260ea62e8664d
 export default Navbar
