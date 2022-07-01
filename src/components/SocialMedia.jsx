3cd94f48ce0138d738a885ae5503aa19174b4093
PatrizioTheDev <patickmedley@gmail.com>
2022-07-01 11:09:12 -0400

    update social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,16 +4,19 @@ import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
+
+
     <div>
       <BsTwitter />
     </div>
+    
     <a href='https://www.facebook.com/profile.php?id=100082245522864'>
     <div> 
-
       <FaFacebookF />
-
     </div>
     </a>
+
+    <a href='https://www.instagram.com/patriziothedev/'>
     <div>
       <BsInstagram />
     </div>
