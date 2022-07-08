bf1829bbf6070932230d7029efe37220519a5a0a
PatrizioTheDev <patickmedley@gmail.com>
2022-07-08 16:55:53 -0400

    Update react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-import { hydrate, render } from "react-dom";
+import { render } from 'react-snapshot';
 
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+render(<App />, document.getElementById("root"));
